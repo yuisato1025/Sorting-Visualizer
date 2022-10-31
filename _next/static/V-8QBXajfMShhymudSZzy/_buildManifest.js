56ba199e021958eb185dce1cb18db2a1cbcb7832
yuisato1025 <yuisato1025@users.noreply.github.com>
2022-10-31 05:40:49 +0000

    deploy: e3b9b4c68dd00d082f4061be1b49facc11adca3d

---
+++ _next/static/V-8QBXajfMShhymudSZzy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/25-21035ce80f16c136.js","static/chunks/pages/index-68f216451de8e617.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
