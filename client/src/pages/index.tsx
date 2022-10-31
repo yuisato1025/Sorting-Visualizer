e3b9b4c68dd00d082f4061be1b49facc11adca3d
Yui Sato <yui_sato@micin.jp>
2022-10-31 14:39:09 +0900

    update: header style

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,56 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import {
+	Text,
+	HStack,
+	Stack,
+	Select,
+	Button,
+	Slider,
+	SliderTrack,
+	SliderFilledTrack,
+	SliderThumb,
+} from "@chakra-ui/react";
+import { FC } from "react";
 
 export default function Index() {
-	return <div>Index</div>;
+	return (
+		<Stack>
+			<Header />
+		</Stack>
+	);
 }
+
+const Header: FC = () => {
+	return (
+		<HStack
+			bg="black"
+			w="100%"
+			px="24px"
+			py="32px"
+			color="white"
+			align="center"
+			justify="space-between"
+		>
+			<HStack w="100%" spacing="16px">
+				<Button color="black">New Array</Button>
+				<Text fontSize="md" whiteSpace="nowrap">
+					Array Size
+				</Text>
+				<Slider aria-label="slider-ex-1" defaultValue={30} w="300px">
+					<SliderTrack>
+						<SliderFilledTrack />
+					</SliderTrack>
+					<SliderThumb />
+				</Slider>
+			</HStack>
+			<HStack>
+				<Select placeholder="Select Method" w="300px">
+					<option value="merge">Merge Sort</option>
+					<option value="quick">Quick Sort</option>
+					<option value="heap">Heap Sort</option>
+					<option value="bubble">Bubble Sort</option>
+				</Select>
+				<Button colorScheme="blue">Sort!</Button>
+			</HStack>
+		</HStack>
+	);
+};
